e676c9dfd055dba46d5df375adf25c12b4d52b79
Pavel Mitin <mitin.pavel@gmail.com>
2013-02-22 14:22:53 +0200

    Introduce Server, ServiceFacade, StorageService, StorageWorker.

--- src/main/scala/com/anahoret/graphit/kv/Server.scala
+++ src/main/scala/com/anahoret/graphit/kv/Server.scala
@@ -1,20 +1,78 @@
 package com.anahoret.graphit.kv
 
 import com.typesafe.config.ConfigFactory
-import akka.actor.{Props, PoisonPill, ActorSystem}
+import akka.actor.Actor
+import akka.actor.Props
+import akka.actor.ActorLogging
+import akka.actor.ActorSystem
+import akka.actor.PoisonPill
+import akka.pattern.ask
 import akka.contrib.pattern.ClusterSingletonManager
+import akka.util.Timeout
+import akka.routing.FromConfig
+import akka.actor.RootActorPath
 
-//case class Get(key: String)
-//case class Put(key: String, value: String)
-//case class Delete(key: String)
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+import akka.routing.ConsistentHashingRouter.ConsistentHashableEnvelope
+import akka.remote.testconductor.TestConductorProtocol.Address
+
+import java.util.concurrent.TimeUnit
+
+case class Get(key: String)
+case class Result(key: String, value: String)
+case class RequestFailed(reason: String)
+
+class StorageService extends Actor {
+  val workerRouter =
+    context.actorOf(Props[StorageWorker].withRouter(FromConfig), name = "storageWorkerRouter")
+
+  def receive = {
+    case Get(key) ⇒ workerRouter.tell(ConsistentHashableEnvelope(key, key), self)
+    case result: Result  ⇒  sender ! result
+  }
+}
+
+class StorageWorker extends Actor {
+  //var cache = Map.empty[String, String]
+
+  def receive = {
+    case Get(key) ⇒ sender ! Result(key, key)
+  }
+}
+
+class ServiceFacade extends Actor with ActorLogging {
+  import context.dispatcher
+  val cluster = Cluster(context.system)
+
+  var currentMaster: Option[akka.actor.Address] = None
+
+  override def preStart(): Unit = cluster.subscribe(self, classOf[LeaderChanged])
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
+  def receive = {
+    case _: Get if currentMaster.isEmpty ⇒
+      sender ! RequestFailed("Service unavailable, try again later")
+    case request: Get ⇒
+      implicit val timeout = Timeout(5, TimeUnit.SECONDS)
+      currentMaster foreach { address ⇒
+        val service = context.actorFor(RootActorPath(address) / "user" / "singleton" / "storageService")
+        service ? request recover {
+          case _ ⇒ RequestFailed("Service unavailable, try again later")
+        }
+      }
+    case state: CurrentClusterState ⇒ currentMaster = state.leader
+    case LeaderChanged(leader)      ⇒ currentMaster = leader
+  }
+}
 
 object Server {
   def main(args: Array[String]): Unit = {
     if (args.nonEmpty) System.setProperty("akka.remote.netty.tcp.port", args(0))
 
-    val system = ActorSystem("GraphitKv", ConfigFactory.parseString("""
+    val system = ActorSystem("ClusterSystem", ConfigFactory.parseString("""
       akka.actor.deployment {
-        /singleton/graphitService/workerRouter {
+        /singleton/storageService/storageWorkerRouter {
             router = consistent-hashing
             nr-of-instances = 100
             cluster {
@@ -24,12 +82,12 @@ object Server {
             }
           }
       }
-      """).withFallback(ConfigFactory.load()))
+    """).withFallback(ConfigFactory.load()))
 
     system.actorOf(Props(new ClusterSingletonManager(
-      singletonProps = _ ⇒ Props[Service], singletonName = "graphitService",
+      singletonProps = _ ⇒ Props[StorageService], singletonName = "storageService",
       terminationMessage = PoisonPill)), name = "singleton")
 
-    system.actorOf(Props[Server], name = "graphitFacade")
+    system.actorOf(Props[ServiceFacade], name = "serviceFacade")
   }
 }
