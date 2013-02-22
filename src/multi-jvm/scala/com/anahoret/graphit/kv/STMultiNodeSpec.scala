a5504ff2a037c20c24c382cc07e80552022901b8
Pavel Mitin <mitin.pavel@gmail.com>
2013-02-22 17:27:24 +0200

    Do run multi-node specs.

---
+++ src/multi-jvm/scala/com/anahoret/graphit/kv/STMultiNodeSpec.scala
@@ -0,0 +1,15 @@
+package com.anahoret.graphit.kv
+
+import org.scalatest.{ BeforeAndAfterAll, WordSpec }
+import org.scalatest.matchers.MustMatchers
+import akka.remote.testkit.MultiNodeSpecCallbacks
+
+/**
+ * Hooks up MultiNodeSpec with ScalaTest
+ */
+trait STMultiNodeSpec extends MultiNodeSpecCallbacks with WordSpec with MustMatchers with BeforeAndAfterAll {
+
+  override def beforeAll() = multiNodeSpecBeforeAll()
+
+  override def afterAll() = multiNodeSpecAfterAll()
+}
