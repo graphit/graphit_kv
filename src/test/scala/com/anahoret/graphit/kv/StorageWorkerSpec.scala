26ccfce529c25d5fd4010c0c1ec0602b8cb7249c
Pavel Mitin <mitin.pavel@gmail.com>
2013-02-22 16:02:48 +0200

    Complete the first test.

--- src/test/scala/com/anahoret/graphit/kv/StorageWorkerSpec.scala
+++ src/test/scala/com/anahoret/graphit/kv/StorageWorkerSpec.scala
@@ -2,31 +2,21 @@ package com.anahoret.graphit.kv
 
 import org.scalatest.{BeforeAndAfterAll, WordSpec}
 import org.scalatest.matchers.MustMatchers
-import akka.testkit.{TestKit, TestActorRef}
+import akka.testkit.{ImplicitSender, TestKit, TestActorRef}
 import akka.actor.{Props, ActorSystem, Actor}
-import java.util.concurrent.TimeUnit
-import actors.threadpool.TimeUnit
-import scala.concurrent.duration._
 
-class StorageWorkerSpec(_system: ActorSystem) extends TestKit(_system)
+class StorageWorkerSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpec with MustMatchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("StorageWorkerSpec"))
 
-  override def afterAll {
-    system.shutdown()
-  }
+  override def afterAll { system.shutdown() }
 
   "A StorageWorker" must {
-
     "answer with the same key" in {
       val worker = system.actorOf(Props[StorageWorker])
-      within(2000 millis) {
-
-        worker ! Get("blah")
-        expectMsg(Result("blah", "blah"))
-      }
+      worker ! Get("blah")
+      expectMsg(Result("blah", "blah"))
     }
   }
 }
-
