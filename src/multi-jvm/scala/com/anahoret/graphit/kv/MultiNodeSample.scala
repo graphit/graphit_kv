0dd4e8e747dfb9eb7328d02ff291d63a8eef5415
Pavel Mitin <mitin.pavel@gmail.com>
2013-02-28 09:36:41 +0200

    Add ConfigFactory to the first multinode spec.

--- src/multi-jvm/scala/com/anahoret/graphit/kv/MultiNodeSample.scala
+++ src/multi-jvm/scala/com/anahoret/graphit/kv/MultiNodeSample.scala
@@ -1,10 +1,18 @@
 package com.anahoret.graphit.kv
 
 import akka.remote.testkit.MultiNodeConfig
+import com.typesafe.config.ConfigFactory
 
 object MultiNodeSampleConfig extends MultiNodeConfig {
   val node1 = role("node1")
   val node2 = role("node2")
+
+  commonConfig(ConfigFactory.parseString("""
+    akka.actor.provider = "akka.cluster.ClusterActorRefProvider"
+    akka.remote.log-remote-lifecycle-events = off
+    akka.cluster.auto-join = off
+    akka.cluster.metrics.collector-class = akka.cluster.JmxMetricsCollector
+  """))
 }
 
 import akka.remote.testkit.MultiNodeSpec
