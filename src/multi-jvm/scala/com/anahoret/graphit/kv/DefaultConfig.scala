b92927a6b15cc6f4f03ad8a4bce7a2a3f446a7fc
Pavel Mitin <mitin.pavel@gmail.com>
2013-03-07 15:22:42 +0200

    Enhance multi-node spec configuration.

--- src/multi-jvm/scala/com/anahoret/graphit/kv/DefaultConfig.scala
+++ src/multi-jvm/scala/com/anahoret/graphit/kv/DefaultConfig.scala
@@ -1,8 +1,41 @@
 package com.anahoret.graphit.kv
 
 import akka.remote.testkit.MultiNodeConfig
+import com.typesafe.config.ConfigFactory
 
 object DefaultConfig extends MultiNodeConfig {
-  val node1 = role("node1")
-  val node2 = role("node2")
+  val graphit1 = role("node1")
+  val graphit2 = role("node2")
+
+  commonConfig(ConfigFactory.parseString("""
+    akka {
+      loglevel = DEBUG
+      stdout-loglevel = DEBUG
+
+      actor {
+        provider = "akka.cluster.ClusterActorRefProvider"
+        debug {
+          receive = on
+          lifecycle = on
+        }
+        deployment {
+          /singleton/storageService/storageWorkerRouter {
+            router = consistent-hashing
+            nr-of-instances = 100
+            cluster {
+              enabled = on
+              max-nr-of-instances-per-node = 3
+              allow-local-routees = off
+            }
+          }
+        }
+      }
+
+      cluster {
+        auto-join = off
+        metrics.collector-class = akka.cluster.JmxMetricsCollector
+      }
+
+      remote.log-remote-lifecycle-events = off
+    }"""))
 }
