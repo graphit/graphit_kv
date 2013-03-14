5e6bea29397b550f3af138d12da4c3f7e4be6b46
Pavel Mitin <mitin.pavel@gmail.com>
2013-03-14 14:04:39 +0200

    Reformat ServerUpSpec.scala.

--- src/multi-jvm/scala/com/anahoret/graphit/kv/ServerUpSpec.scala
+++ src/multi-jvm/scala/com/anahoret/graphit/kv/ServerUpSpec.scala
@@ -26,45 +26,45 @@ class ServerUpSpec extends MultiNodeSpec(DefaultConfig) with STMultiNodeSpec wit
 
   "Server" should {
     "be up" in {
-        Cluster(system).subscribe(testActor, classOf[MemberUp])
-        expectMsgClass(classOf[CurrentClusterState])
+      Cluster(system).subscribe(testActor, classOf[MemberUp])
+      expectMsgClass(classOf[CurrentClusterState])
 
-        val graphit1Address = node(graphit1).address
-        val graphit2Address = node(graphit2).address
+      val graphit1Address = node(graphit1).address
+      val graphit2Address = node(graphit2).address
 
-        Cluster(system) join graphit2Address
+      Cluster(system) join graphit2Address
 
-        runOn(graphit1) {
-          Server.createActors(system)
-          testConductor.enter("gpaphit1-up")
-        }
-
-        runOn(graphit2) {
-          testConductor.enter("gpaphit1-up")
-          Server.createActors(system)
-        }
-
-        expectMsgAllOf(
-          MemberUp(Member(graphit1Address, MemberStatus.Up)),
-          MemberUp(Member(graphit2Address, MemberStatus.Up)))
-
-        Cluster(system).unsubscribe(testActor)
-
-        testConductor.enter("all-up")
+      runOn(graphit1) {
+        Server.createActors(system)
+        testConductor.enter("gpaphit1-up")
       }
 
-      "put and get key-value pairs" in {
-        runOn(graphit1) {
-          val facade = system.actorFor("user/serviceFacade")
-
-          facade ! Put("my-key", "my-value")
-
-          val future = facade ? Get("my-key")
-          val result = Await.result(future, timeout.duration).asInstanceOf[Result]
-          result should be(Result("my-key", Some("my-value")))
-        }
-
-        testConductor.enter("done-test1")
+      runOn(graphit2) {
+        testConductor.enter("gpaphit1-up")
+        Server.createActors(system)
       }
+
+      expectMsgAllOf(
+        MemberUp(Member(graphit1Address, MemberStatus.Up)),
+        MemberUp(Member(graphit2Address, MemberStatus.Up)))
+
+      Cluster(system).unsubscribe(testActor)
+
+      testConductor.enter("all-up")
+    }
+
+    "put and get key-value pairs" in {
+      runOn(graphit1) {
+        val facade = system.actorFor("user/serviceFacade")
+
+        facade ! Put("my-key", "my-value")
+
+        val future = facade ? Get("my-key")
+        val result = Await.result(future, timeout.duration).asInstanceOf[Result]
+        result should be(Result("my-key", Some("my-value")))
+      }
+
+      testConductor.enter("done-test1")
+    }
   }
 }
