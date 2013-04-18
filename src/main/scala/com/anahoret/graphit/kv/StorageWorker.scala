c3ead14b53cc65b505bf842b7aa9731415c1364a
Pavel Mitin <mitin.pavel@gmail.com>
2013-04-18 14:06:17 +0300

    Cleanup: remove redundant log messages.

--- src/main/scala/com/anahoret/graphit/kv/StorageWorker.scala
+++ src/main/scala/com/anahoret/graphit/kv/StorageWorker.scala
@@ -3,11 +3,10 @@ package com.anahoret.graphit.kv
 import scala.collection.mutable
 import akka.actor.{ActorLogging, Actor}
 
-class StorageWorker(storage: mutable.Map[String, String] = mutable.Map.empty[String, String]) extends Actor with ActorLogging {
-
+class StorageWorker(storage: mutable.Map[String, String] = mutable.Map.empty[String, String]) extends Actor {
   def receive = {
     case Get(key) => sender ! lookup(key)
-    case Put(key, value) => log.error("StorageWorker PUT {} {})", key, value); store(key, value)
+    case Put(key, value) => store(key, value)
   }
 
   private def lookup(key: String): Result = Result(key, storage.get(key))
